66a42315a612d653f48962ec5bdeda0a7bfe9b2e
Miłosz Domagalski <milosz.domagalaki@gmail.com>
2025-08-10 12:22:47 +0200

    Created an endpoint which allows user to check values of a todolist through get

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-items = []
+to_do_list = [
+    {"todo_id" : 1, "todo_name" : "Sport", "todo_description" : "Go to the gym"},
+    {"todo_id" : 2, "todo_name" : "Meditate", "todo_description" : "Meditate for 20 minutes"},
+    {"todo_id" : 3, "todo_name" : "Gaming", "todo_description" : "Play a video game"},
+    {"todo_id" : 4, "todo_name" : "Tournament", "todo_description" : "Play a basketball tournament"},
+    {"todo_id" : 5, "todo_name" : "Cinema", "todo_description" : "Watch a new movie"},
+
+ ]
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
-@app.post("/items")
-def create_item(item: str):
-    items.append(item)
-    return items
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
-    item = items[item_id]
-    return item
+@app.get("/todo/{todo_id}")
+def get_item(todo_id: int):
+    for todo in to_do_list:
+        if todo["todo_id"] == todo_id:
+            return todo
+    
+
+
+    return {"error" : "Item not found"}
+
+
+        
+
+
+
